827c734f275052793b11a2f519d6d7d8a908d78e
Shlomo Gordon <sgordon16@student.touro.edu>
2018-10-12 00:02:13 -0400

    changed program to add two numbers

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,9 +1,16 @@
+import java.util.Scanner;
 
 public class HelloWorld {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Hello World");
+		Scanner kb = new Scanner(System.in);
+		System.out.println("Enter number 1");
+		int a = kb.nextInt();
+		System.out.println("Enter number 2");
+		int b = kb.nextInt();
+		System.out.println(a+b);
+		kb.close();
 	}
 
 }
