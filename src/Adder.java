7b1c1b5d9284dec41419449a53a286b1098401d1
Shlomo Gordon <sgordon16@student.touro.edu>
2018-10-12 00:14:09 -0400

    added Adder class

---
+++ src/Adder.java
@@ -0,0 +1,17 @@
+
+public class Adder {
+	
+	private int a;
+	private int b;
+	
+	public Adder(int a, int b)
+	{
+		this.a = a;
+		this.b = b;
+	}
+	public int add()
+	{
+		return a + b;
+	}
+     
+}
