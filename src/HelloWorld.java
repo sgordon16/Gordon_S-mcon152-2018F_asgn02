b7165cc7a74b2407458fd6d80da03716697a462e
Shlomo Gordon <sgordon16@student.touro.edu>
2018-10-12 01:21:14 -0400

    created Calculater class, modified main program, deleted Adder and Subtracter classes

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -5,14 +5,26 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		
 		Scanner kb = new Scanner(System.in);
-		System.out.println("Enter number 1");
-		int a = kb.nextInt();
-		System.out.println("Enter number 2");
-		int b = kb.nextInt();
-		Adder adder = new Adder(a,b);
-		Subtracter  subtracter = new Subtracter(a,b);
-		System.out.println(adder.add());
-		System.out.println(subtracter.subtract());
+		char choice;
+		int a;
+		int b;
+		
+		do {
+			System.out.println("Enter number 1");
+			a = kb.nextInt();
+			System.out.println("Enter number 2");
+			b = kb.nextInt();
+			kb.nextLine();
+			Calculater calculater = new Calculater(a, b);
+			System.out.println("Do you want to (a); add or (b) subtract?");
+			choice = kb.nextLine().charAt(0);
+			if(choice == 'a')
+				System.out.println(calculater.add());
+			if(choice == 'b')
+				System.out.println(calculater.subtract());
+			System.out.println("Enter 'q' to quit or any other key to keep calculating");
+			choice = kb.nextLine().charAt(0);
+		} while(choice != 'q');
 		kb.close();
 	}
 
