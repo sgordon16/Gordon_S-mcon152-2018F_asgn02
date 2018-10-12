2a9dedc251c45a50fc7b8bf91ffeeacaf6199653
Shlomo Gordon <sgordon16@student.touro.edu>
2018-10-12 01:13:35 -0400

    created Calculater class, adjusted main program, deleted Adder and Subtracter classes

---
+++ src/Calculater.java
@@ -0,0 +1,20 @@
+
+public class Calculater {
+	
+	private int a;
+	private int b;
+	
+	public Calculater(int a, int b) {
+		
+		this.a = a;
+		this.b = b;
+	}
+	public int add() {
+		
+		return a + b;
+	}
+	public int subtract() {
+		
+		return a - b;
+	}
+}
